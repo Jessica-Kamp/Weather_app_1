33f5b76495a2a2df280305d56d467c0bb7e09cca
Jessica-Kamp <jessicakamphuis@gmail.com>
2020-12-18 20:05:01 +0800

    Fixed conversion, deleted duplicate functions

--- src/Index.js
+++ src/Index.js
@@ -1,13 +1,10 @@
 function formatDate(date) {
   return `${days[dayIndex]}, ${hours}:${minutes}`;
 }
-
 let dateElement = document.querySelector("#date");
-
 let date = new Date();
 let dayIndex = date.getDay();
 let hours = date.getHours();
-
 if (hours < 10) {
   hours = `0${hours}`;
 }
@@ -16,79 +13,51 @@ if (minutes < 10) {
   minutes = `0${minutes}`;
 }
 let days = ["Sun", "Mon", "Tue", "Wed", "Thur", "Fri", "Sat"];
-
 dateElement.innerHTML = `${days[dayIndex]}, ${hours}:${minutes}`;
-
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
   let conditionElement = document.querySelector("#condition");
   let dateElement = document.querySelector("#date");
-  
-
-function showTemperature(response) {
-  console.log(response);
-  console.log(response.data.main.temp);
-  let temperature = Math.round(response.data.main.temp);
-
-  let h2 = document.querySelector("h2");
-  h2.innerHTML = `${temperature}°C`;
-}
-
-function searchCity(city) {
-  let apiKey = "52ab87c28c3fa62d2df55c9dfce87bc7";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-  axios.get(apiUrl).then(displayWeatherCondition);
-}
-
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
   celsiusTemperature = response.data.main.temp;
-
   temperatureElement.innerHTML = Math.round(celsiusTemperature);
   cityElement.innerHTML = response.data.name;
-  conditionElement.innerHTML = response.data.weather[0].condition;
+  conditionElement.innerHTML = response.data.weather[0].description;
   humidityElement.innerHTML = response.data.main.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
-  }
-
+}
 function searchCity(city) {
   let apiKey = "52ab87c28c3fa62d2df55c9dfce87bc7";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-  axios.get(apiUrl).then(displayForecast);
+  axios.get(apiUrl).then(displayTemperature);
 }
-
 function handleSubmit(event) {
   event.preventDefault();
   let city = document.querySelector("#city-input").value;
   searchCity(city);
 }
-
-
 function handlePosition(position) {
   console.log(position.coords.latitude);
   console.log(position.coords.longitude);
-
   let latitude = position.coords.latitude;
   let longitude = position.coords.longitude;
   let units = "metric";
   let apiKey = "52ab87c28c3fa62d2df55c9dfce87bc7";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
-
   axios.get(apiUrl).then(showTemperature);
 }
-
 navigator.geolocation.getCurrentPosition(handlePosition);
-
 function displayFahrenheitTemperature(event) {
   event.preventDefault();
   let temperatureElement = document.querySelector("#temperature");
-
   celsiusLink.classList.remove("active");
   fahrenheitLink.classList.add("active");
   let fahrenheiTemperature = (celsiusTemperature * 9) / 5 + 32;
   temperatureElement.innerHTML = Math.round(fahrenheiTemperature);
 }
-
 function displayCelsiusTemperature(event) {
   event.preventDefault();
   celsiusLink.classList.add("active");
@@ -96,17 +65,11 @@ function displayCelsiusTemperature(event) {
   let temperatureElement = document.querySelector("#temperature");
   temperatureElement.innerHTML = Math.round(celsiusTemperature);
 }
-
 let celsiusTemperature = null;
-
-
 let searchForm = document.querySelector("#search-form");
 searchForm.addEventListener("submit", handleSubmit);
-
 let fahrenheitLink = document.querySelector("#fahrenheit-link");
 fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
-
 let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
-
-search("Hong Kong");
+searchCity("Hong Kong");
